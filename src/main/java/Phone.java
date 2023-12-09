74e78829dd9a1ca79b1e8a5546c408880830b0a3
JamesDoherty1 <145557649+JamesDoherty1@users.noreply.github.com>
2023-12-09 19:27:31 +0000

    mergin TaxiAppGUI with Phone class

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -1,32 +1,62 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Phone extends JFrame implements ActionListener {
 
     private MapPanel mapPanel;
+    private ImageIcon backgroundImage = new ImageIcon("src/main/java/TaxiApp.png");
+    private JLabel backgroundLabel = new JLabel(backgroundImage);
+    private JLabel question;
+    private JTextArea driverInfo;
+    private JButton redButton;
+    private JButton blueButton;
+    private JButton yellowButton;
 
     public Phone(MapPanel mapPanel) {
         this.mapPanel = mapPanel;
-        setSize(300, 200);
+        this.add(backgroundLabel);
+        this.setContentPane(backgroundLabel);
+        setSize(429, 762);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        setLocation(1025, 30);
+
+        question = new JLabel("Where would you like to go?");
+        this.add(question);
+        question.setBounds(75, 100, 300, 80);
+        question.setBackground(Color.BLACK);
+        question.setFont(new Font("Fugaz One", Font.PLAIN, 20));
+
+        driverInfo = new JTextArea(" \n Name: John \n \n Car Type: Volkswagen  \n \n License Plate: 123  \n \n Rating: 3 stars");
+        this.add(driverInfo);
+        driverInfo.setEditable(false);
+        driverInfo.setBounds(60, 200, 300, 300);
+        driverInfo.setBackground(Color.GREEN);
+        driverInfo.setFont(new Font("Fugaz One", Font.PLAIN, 20));
+        driverInfo.setVisible(false);
 
         // Create buttons
-        JButton redButton = new JButton("Red");
+        redButton = new JButton("Red");
+        this.add(redButton);
+        redButton.setBounds(100, 200, 200, 80);
+        redButton.setBackground(Color.RED);
+        redButton.setFont(new Font("Fugaz One", Font.PLAIN, 40));
         redButton.addActionListener(this);
 
-        JButton blueButton = new JButton("Blue");
+        blueButton = new JButton("Blue");
+        this.add(blueButton);
+        blueButton.setBounds(100, 350, 200, 80);
+        blueButton.setBackground(Color.BLUE);
+        blueButton.setFont(new Font("Fugaz One", Font.PLAIN, 40));
         blueButton.addActionListener(this);
 
-        JButton yellowButton = new JButton("Yellow");
+        yellowButton = new JButton("Yellow");
+        this.add(yellowButton);
+        yellowButton.setBounds(100, 500, 200, 80);
+        yellowButton.setFont(new Font("Fugaz One", Font.PLAIN, 40));
+        yellowButton.setBackground(Color.YELLOW);
         yellowButton.addActionListener(this);
-
-        // Add the JButtons to the JFrame
-        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        getContentPane().add(redButton);
-        getContentPane().add(blueButton);
-        getContentPane().add(yellowButton);
 
         setVisible(true);
     }
@@ -46,5 +76,12 @@ public class Phone extends JFrame implements ActionListener {
                 Collection.getClosestTaxi(mapPanel.getTaxis(), "yellow");
                 break;
         }
+
+        // Update UI after button press
+        question.setText("Drivers Information");
+        redButton.setVisible(false);
+        blueButton.setVisible(false);
+        yellowButton.setVisible(false);
+        driverInfo.setVisible(true);
     }
 }
